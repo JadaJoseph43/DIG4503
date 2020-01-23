80774d99020197fe3ebff2699cea8de6f8a3c494
JadaJoseph43 <42987213+JadaJoseph43@users.noreply.github.com>
2020-01-23 12:32:59 -0500

    Changes to new Lab 2

---
+++ lab2/src/Componets/HomePage/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Homepage extends React.Component {
+    render(){
+        return(
+            <p>{this.props.FirstName}</p>
+        );
+    }
+}
+export default Homepage;
