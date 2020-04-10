bb638694b13eeaeaa732cecf664c21cc78342e6f
JadaJoseph43 <42987213+JadaJoseph43@users.noreply.github.com>
2020-04-10 15:32:32 -0400

    start

---
+++ docs/pages/SearchID.js
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../components/CSS/CSS.module.css';
+import "./api/pokemon/id/[id].js";
+import IdSearch from "../components/IdSearch/index.js";
+
+const SearchID = () => {
+  return (
+      <div className={styles.body}>
+        <Head>
+          <title>Pokemon Database</title>
+        </Head>
+        <div>
+          <h1 className={styles.head}>Pokemon Database</h1>
+          <p>
+            Search for a pokemon by ID
+          </p>
+          <p><IdSearch /></p>
+          <p><Link href="SearchName">
+            <a className={styles.a}>Search by Name</a>
+          </Link></p>
+          <p><Link href="/SearchType">
+            <a className={styles.a}>Search by Type</a>
+          </Link></p>
+        </div>
+      </div>
+    );
+  }
+
+export default SearchID;
---
+++ docs/pages/SearchType.js
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../components/CSS/CSS.module.css';
+import "./api/pokemon/type/[type].js";
+import TypeSearch from "../components/TypeSearch/index.js";
+
+const SearchType = () => {
+  return (
+      <div className={styles.body}>
+        <Head>
+          <title>Pokemon Database</title>
+        </Head>
+        <div>
+          <h1 className={styles.head}>Pokemon Database</h1>
+          <p>
+            Search for a pokemon by type
+          </p>
+          <p><TypeSearch /></p>
+          <p><Link href="SearchName">
+            <a className={styles.a}>Search by Name</a>
+          </Link></p>
+          <p><Link href="/SearchID">
+            <a className={styles.a}>Search by ID</a>
+          </Link></p>
+        </div>
+      </div>
+    );
+  }
+
+export default SearchType;
